a26cf0115b992e0b464b2e2e1653111b4ecc6455
Lachlan Rixon <lachlan_rixon@icloud.com>
2025-07-09 12:29:58 +1000

    added group infrastructure

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable= pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
@@ -18,8 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawings in drawable:
+            drawings.draw(screen)
         pygame.display.flip()
 
         # Limits framerate to 60fps
