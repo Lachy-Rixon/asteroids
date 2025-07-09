08b266076782ccff7fdc07bd85bc898ff8599cb3
Lachlan Rixon <lachlan_rixon@icloud.com>
2025-07-09 11:45:16 +1000

    Added fps clock

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +18,8 @@ def main():
         screen.fill((0,0,0))
         pygame.display.flip()
 
+        # Limits framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
